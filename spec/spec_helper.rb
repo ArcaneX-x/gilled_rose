e6edef5f5018c790d622bd96671baf057f495f95
Evgeny Okhrimenko <fasolshik@gmail.com>
2023-10-23 12:08:25 +0600

    removed garbage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,6 @@ require_relative '../lib/gilded_require'
 
 RSpec.configure do |config|
   # config.example_status_persistence_file_path = 'spec/specs.txt'
-  # config.define_derived_metadata(file_path: /exchange_it\/utils/) do |meta|
-  #   meta[:fast] = true
-  # end
 
   # config.filter_run_excluding :win_only unless RUBY_PLATFORM.match?(/jruby/i)
 end
